0599ed68d28d93bede78e3d456aa75dda276597d
jorrittempelman <jorrit.tempelman@iqmedia.nl>
2019-03-19 08:56:35 +0100

    Updates

---
+++ precache-manifest.46d160df33cb75c5c16826af3d738db5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "237aa5cf2b5d37845026",
+    "url": "/tabchat/static/css/main.5705a124.chunk.css"
+  },
+  {
+    "revision": "237aa5cf2b5d37845026",
+    "url": "/tabchat/static/js/main.ff4e4b97.chunk.js"
+  },
+  {
+    "revision": "caa760e5b018062b73c4",
+    "url": "/tabchat/static/js/runtime~main.0266a0f8.js"
+  },
+  {
+    "revision": "5fae2fa602b3cea2ed91",
+    "url": "/tabchat/static/js/2.bde6d161.chunk.js"
+  },
+  {
+    "revision": "57a052e38a4459b43dd52af98a3dc458",
+    "url": "/tabchat/static/media/tabchat-logo.57a052e3.png"
+  },
+  {
+    "revision": "7e410d1ea9b7aee40e1e783c36f1bf95",
+    "url": "/tabchat/index.html"
+  }
+];
