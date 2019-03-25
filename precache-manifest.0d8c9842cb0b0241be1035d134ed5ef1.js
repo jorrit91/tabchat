e149c7f31bab410b67f29ae0a3954df4c9aed9bb
Jorrit Tempelman <tempelmanjorrit@gmail.com>
2019-03-25 20:50:37 +0100

    Updates

---
+++ precache-manifest.0d8c9842cb0b0241be1035d134ed5ef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "57a052e38a4459b43dd52af98a3dc458",
+    "url": "/tabchat/static/media/tabchat-logo.57a052e3.png"
+  },
+  {
+    "revision": "caa760e5b018062b73c4",
+    "url": "/tabchat/static/js/runtime~main.0266a0f8.js"
+  },
+  {
+    "revision": "bd950520e038584cb504",
+    "url": "/tabchat/static/js/main.dfb4ed5f.chunk.js"
+  },
+  {
+    "revision": "b9c03a6c339d697553d1",
+    "url": "/tabchat/static/js/2.e92ddf1c.chunk.js"
+  },
+  {
+    "revision": "bd950520e038584cb504",
+    "url": "/tabchat/static/css/main.c2168640.chunk.css"
+  },
+  {
+    "revision": "0c44fbe952f2b0bfcef2a1303c4c56b0",
+    "url": "/tabchat/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tabchat/build/precache-manifest.6741812494e8f4ce0aa401deafdec6be.js"
+  "/tabchat/precache-manifest.0d8c9842cb0b0241be1035d134ed5ef1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tabchat/build/index.html", {
+workbox.routing.registerNavigationRoute("/tabchat/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
